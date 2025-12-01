655520d576db3d72ed26006bb7dff06deead6c42
NeyBg23 <mathicuenta23@gmail.com>
2025-12-01 02:49:25 -0500

    fix: importar authRoutes.js correctamente en index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import authRoutes from './api/routes/index.js'; // importa tus rutas
+import authRoutes from './api/routes/authRoutes.js'; // importa tus rutas
 
 dotenv.config();
 const app = express();
