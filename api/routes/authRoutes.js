44c39c201ed58842759db946e71649465a632845
NeyBg23 <mathicuenta23@gmail.com>
2025-12-02 01:55:46 -0500

    agregando express en auth

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,10 +1,17 @@
+// api/routes/authRoutes.js
+import { Router } from 'express';                // 👈 IMPORTANTE
 import { registro, login, validate } from '../controllers/authController.js';
 import { verifyToken } from '../middleware/jwtValidate.js';
 
 const router = Router();
 
+// Registro
 router.post('/registro', registro);
+
+// Login
 router.post('/login', login);
+
+// Validar token
 router.get('/validate', verifyToken, validate);
 
 export default router;
